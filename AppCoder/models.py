afc042176609db263795708bd0290c8de13b6297
Bautista Moreno <bautiistam10@outlook.com>
2023-02-15 18:11:19 -0300

    Clase-20 Completa

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -6,11 +6,17 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
 
+    def __str__(self):
+        return self.nombre + '(' + str(self.camada) + ')'
+
 class Estudiante(models.Model):
 
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     email = models.EmailField()
+
+    def __str__(self):
+        return self.nombre + ' ' + str(self.apellido)  
 
 class Profesor(models.Model):
 
@@ -19,8 +25,17 @@ class Profesor(models.Model):
     email = models.EmailField()
     profesion = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.nombre + ' ' + str(self.apellido) 
+
+    class Meta:
+        verbose_name_plural = 'Profesores'
+
 class Entregable(models.Model):
 
     nombre = models.CharField(max_length=30)
     fecha_entrega = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self):
+        return self.nombre 
